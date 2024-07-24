8d65ceeb40e2d5478d830f6372ca53ffa68614fa
khoadong07 <khoadong07@gmail.com>
2024-07-24 21:19:49 +0700

    fix cors

--- main.py
+++ main.py
@@ -3,18 +3,28 @@ from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer, util
 import json
 import torch
+from starlette.middleware.cors import CORSMiddleware
 
 from find_keyword import find_related_keyword
 from response_template import bad_request, success
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-# Đọc danh sách từ khóa từ tệp JSON
 def load_keywords(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
+
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = SentenceTransformer('paraphrase-mpnet-base-v2', device=device)
@@ -22,6 +32,7 @@ model = SentenceTransformer('paraphrase-mpnet-base-v2', device=device)
 flat_keywords = load_keywords('keywords/beauty_skincare.json')
 
 keyword_embeddings = {keyword: model.encode(keyword, convert_to_tensor=True) for keyword in flat_keywords}
+
 
 def get_embedding(text):
     return model.encode(text, convert_to_tensor=True)
@@ -55,8 +66,10 @@ def find_related_keyword(sentence):
 
     return related_keyword
 
+
 class Query(BaseModel):
     query: str
+
 
 @app.post("/api/find-keyword")
 async def find_keyword(query: Query):
@@ -75,7 +88,8 @@ async def find_keyword(query: Query):
     else:
         return bad_request(message="Keyword not found", data=None)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
